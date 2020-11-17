c69a2d2e8aaccad4e2fc3e94615cda07c30f494b
Jordan Phan <JordanJPhan@gmail.com>
2020-11-16 17:27:54 -0800

    redo binary search

--- AlgoExpert/BinarySearch.js
+++ AlgoExpert/BinarySearch.js
@@ -1,19 +1,33 @@
+// High Level Solution:
+		// create a middle pointer
+		// check to see if target is smaller or bigger than pointer
+		// place left and right pointers
+			// left points to the first index 
+			// right points to last index
+			// re-assign middle to left + right / 2 => floor it
+			
+// array [ 0 1 21 33 45 ]
+// array [ 45 61 71 72 74] 
+// left = 7
+// right = 9
+// middle = 7 = 72
+// target = 73
+
 function binarySearch(array, target) {
-    return binarySearchHelper(array, target, 0, array.length - 1)
-    
+	let left = 0;
+	let right = array.length - 1;
+	let middle 
+	while (left <= right) {
+		
+		middle = Math.floor((left + right) / 2)
+		if (array[middle] === target) {
+			return middle
+		} else if (array[middle] > target ) {
+			right = middle - 1
+		} else if (array[middle] < target ) {
+			left = middle + 1
+		}
+	}
+	return -1
 }
 
-function binarySearchHelper(array, target, left, right) {
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2)
-        const potMatch = array[middle] 
-        if (target === potMatch) {
-            return mid
-        } else if (target < potMatch) {
-            right = mid - 1
-        } else {
-            left = mid + 1
-        }
-    }
-    return -1; 
-}
